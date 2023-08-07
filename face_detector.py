2329725d553ce7dbb7f03685b3adff7f6637436e
Naufal Zhafif <98691805+naufalzp@users.noreply.github.com>
2023-08-07 07:47:02 +0700

    Add files via upload

---
+++ face_detector.py
@@ -0,0 +1,53 @@
+import cv2
+
+def detect_faces_and_eyes():
+    # Load the pre-trained face and eye detection models from OpenCV
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+    # Access the integrated webcam (index 0)
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Reset person counter for each frame
+        person_counter = 0
+
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        
+        # Flip the frame horizontally to create a mirror effect
+        frame = cv2.flip(frame, 1)
+
+        # Convert the frame to grayscale for face detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the frame
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        # Draw rectangles around the detected faces and eyes and label persons
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = frame[y:y + h, x:x + w]
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+            # Label the person
+            person_counter += 1
+            label = f"Person {person_counter}"
+            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+        # Display the resulting frame with face and eye detections
+        cv2.imshow('Face and Eye Detection', frame)
+
+        # Exit the loop if the 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the video capture and close the window
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    detect_faces_and_eyes()
